d19a7a3f10ed49fba1bbe3f1ce52e41a77e786ca
Josh Loya <116465444+loyaj@users.noreply.github.com>
2023-05-06 18:44:10 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'IonItemSliding open repro',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
